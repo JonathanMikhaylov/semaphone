44b3be5bc4c6b2b97d626c08305abadf1294ad22
jhua1 <jhua@stuy.edu>
2016-12-09 15:35:00 -0500

    most of control done

--- semaphone.c
+++ semaphone.c
@@ -5,7 +5,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <sys/shm.h>
 
+//char *story weird string address
 union semun{
   int val; 
   struct seld_ds* bus;
@@ -16,7 +19,8 @@ union semun{
 int main(int argc, char *argv[] ) {
   int semid;
   int key = ftok("semaphone.c", 22);
-  int sc;
+  int sc,sd,fd;
+
   if (strncmp(argv[1], "-c", strlen(argv[1])) == 0 ) {
     semid = semget(key, 1, IPC_CREAT | IPC_EXCL | 0644);
     printf("semaphore created: %d\n", semid);
@@ -24,16 +28,38 @@ int main(int argc, char *argv[] ) {
     su.val = 1;
     sc = semctl(semid, 0, SETVAL, su);
     printf("value set: %d\n", sc);
+    
+    //file writing
+    fd = open("story",O_CREAT | O_TRUNC, 0644);
+    close(fd);
+    //Shared Memory
+    sd = shmget(key,256 , IPC_CREAT | IPC_EXCL);    
   }
   else if (strncmp(argv[1], "-v", strlen(argv[1])) == 0){
     semid = semget(key, 1, 0);
     sc = semctl(semid, 0, GETVAL);
     printf("semaphore value: %d\n", sc);
+    //read story
+    fd = open("story",O_RDONLY, 0644);
+    char *story;
+    read(fd,story,100);
+    close(fd);
+    printf("Story:\n %s\n", story);
   }
   else if(strncmp(argv[1], "-r", strlen(argv[1])) == 0) {
+    //Printing Story
+    fd = open("story",O_RDONLY, 0644);
+    char *story;
+    read(fd,story,100);
+    close(fd);
+    printf("Story:\n %s\n", story);
+    //Removing Semaphore
     semid = semget(key, 1, 0);
     sc = semctl(semid, 0, IPC_RMID);
     printf("semaphore removed: %d\n", sc);
+    //Removing Shared Memory
+    sd = shmget(key,256,0644);    
+    shmctl(sd,IPC_RMID,0);
   }
   return 0;
 }
